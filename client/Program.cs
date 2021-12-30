02d6a9935dd6272d8bdcf94160774020e59e3baf
sharath <sharathgopinathrao@gmail.com>
2021-12-30 22:12:50 +1100

    Server side streaming rpc

--- client/Program.cs
+++ client/Program.cs
@@ -1,14 +1,69 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 using Grpc.Net.Client;
 using GrpcDemo.Client;
 using static GrpcDemo.Client.GrpcDemo;
 
-// The port number must match the port of the gRPC server.
-using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+namespace Grpc.Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // The port number must match the port of the gRPC server.
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new GrpcDemoClient(channel);
 
-var client = new GrpcDemoClient(channel);
-var reply = await client.SayHelloAsync(
-                  new HelloRequest { Name = "GrpcClient" });
-Console.WriteLine("Greeting: " + reply.Message);
-Console.WriteLine("Press any key to exit...");
-Console.ReadKey();
+            var selectedOption = 0;
+            do
+            {
+                switch (selectedOption)
+                {
+                    case 1:
+                        await SayHello(client);
+                        break;
+
+                    case 2:
+                        await GetTextStream(client);
+                        break;
+
+                    default: break;
+                };
+                selectedOption = DisplayOptions();
+            } while (selectedOption < 3);
+        }
+
+        private static int DisplayOptions()
+        {
+            Console.WriteLine("\r\nSelect an option:");
+            Console.WriteLine("1. SayHello");
+            Console.WriteLine("2. GetTextStream");
+            Console.WriteLine("3. Exit");
+
+            var selectedOption = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+            return int.Parse(selectedOption.ToString());
+        }
+
+        private static async Task GetTextStream(GrpcDemoClient client)
+        {
+            using var call = client.GetTextStream(new EmptyParams());
+            var responseStream = call.ResponseStream;
+
+            Console.WriteLine("Response: ");
+            while(await responseStream.MoveNext(new CancellationToken()))
+            {
+                var line = responseStream.Current;
+                Console.WriteLine(line.ToString());
+            }
+        }
+
+        private static async Task SayHello(GrpcDemoClient client)
+        {
+            var reply = await client.SayHelloAsync(
+                          new HelloRequest { Name = "GrpcClient" });
+            Console.WriteLine("Greeting: " + reply.Message);
+        }
+    }
+}
--- server/Services/GrpcDemoService.cs
+++ server/Services/GrpcDemoService.cs
@@ -1,9 +1,13 @@
+using System.Threading;
 using System.Threading.Tasks;
 using Grpc.Core;
 using Microsoft.Extensions.Logging;
 
 namespace GrpcDemo.Server
 {
+    /// <summary>
+    /// Server implementation of grpc-demo.proto
+    /// </summary>
     public class GrpcDemoService : GrpcDemo.GrpcDemoBase
     {
         private readonly ILogger<GrpcDemoService> _logger;
@@ -12,12 +16,46 @@ namespace GrpcDemo.Server
             _logger = logger;
         }
 
+        /// <summary>
+        /// A simple Unary RPC that responds with a greeting message
+        /// </summary>
+        /// <param name="request">Name of the client</param>
+        /// <param name="context"></param>
+        /// <returns>A greeting message</returns>
         public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
         {
             return Task.FromResult(new HelloReply
             {
                 Message = "Hello " + request.Name
             });
+        }
+
+        /// <summary>
+        /// A server-side streaming RPC where the client sends a request to the server and gets a stream to read a sequence of messages back
+        /// </summary>
+        /// <param name="request">Empty params</param>
+        /// <param name="responseStream">Stream of messages</param>
+        /// <param name="context"></param>
+        /// <returns>A response stream</returns>
+        public override async Task GetTextStream(EmptyParams request,
+            IServerStreamWriter<TextStreamResponse> responseStream,
+            ServerCallContext context)
+        {
+            var lines = new string[] { 
+                "Sphinx of black quartz, judge my vow",
+                "The early bird catches the worm",
+                "The second mouse gets the cheese",
+                "The first mouse gets the cheese",
+                "The third mouse gets the cheese",
+                "The fourth mouse gets the cheese", };
+
+            foreach (var line in lines)
+            {
+                await responseStream.WriteAsync(new TextStreamResponse { Line = line });
+
+                // Sleeps for 1 second so that the streaming response is clearly noticeable from the console client
+                Thread.Sleep(1000);
+            }
         }
     }
 }
